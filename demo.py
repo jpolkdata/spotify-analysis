88db2f8119d71c74d62be286ed93d99e03eea307
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2022-11-27 09:56:49 -0600

    initial commit, demo script that gets top 10 tracks for an artist

---
+++ demo.py
@@ -0,0 +1,21 @@
+import spotipy
+
+spotify = spotipy.Spotify(client_credentials_manager=spotipy.oauth2.SpotifyClientCredentials())
+
+def get_artist_uri(name):
+    results = spotify.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    artist_uri = items[0]['uri']
+    return artist_uri
+
+def top_10_tracks(uri):
+    results = spotify.artist_top_tracks(uri)
+    for track in results['tracks'][:10]:
+        print('track    : ' + track['name'])
+        print('audio    : ' + track['preview_url'])
+        print('cover art: ' + track['album']['images'][0]['url'])
+        print()
+
+if __name__ == "__main__":
+    uri = get_artist_uri("Led Zeppelin")
+    top_10_tracks(uri)
