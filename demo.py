228044a1e9c143322188dd3fce187422955058f7
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2022-11-27 20:51:43 -0600

    added functionality to get related artists

--- demo.py
+++ demo.py
@@ -8,14 +8,23 @@ def get_artist_uri(name):
     artist_uri = items[0]['uri']
     return artist_uri
 
-def top_10_tracks(uri):
+def top_tracks(uri):
     results = spotify.artist_top_tracks(uri)
-    for track in results['tracks'][:10]:
+    for track in results['tracks'][:5]:
         print('track    : ' + track['name'])
         print('audio    : ' + track['preview_url'])
         print('cover art: ' + track['album']['images'][0]['url'])
         print()
 
+def get_related_artists(uri):
+    artists = spotify.artist_related_artists(uri)
+    for artist in artists['artists'][:3]:
+        print('artist       :' + artist['name'])
+        print('popularity   :' + str(artist['popularity']))
+        print('followers    :' + str(artist['followers'].get('total')))
+        print()
+
 if __name__ == "__main__":
-    uri = get_artist_uri("Led Zeppelin")
-    top_10_tracks(uri)
+    uri = get_artist_uri("Coheed and Cambria")
+    top_tracks(uri)
+    get_related_artists(uri)
