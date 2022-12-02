804cfcc0640ff39ec910a9ea1cbb1c5291719e0c
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2022-12-02 10:24:40 -0600

    get just the year that the album was released, output the final dataset to a dict

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Spotify Data Analysis
-This app interacts with the Spotify API to determine how album lenghts have changed over time. The data analysis starts by evaluating a playlist. We take all the artists from that playlist, calculate each of those artists' album lengths, and output some reporting to visualize our findings.
+This app interacts with the Spotify API to determine how album lengths have changed over time. The data analysis starts by evaluating a playlist. We take all the artists from that playlist, calculate each of those artists' album lengths, and output some reporting to visualize our findings.
--- get_album_avg_length_playlist.py
+++ get_album_avg_length_playlist.py
@@ -41,13 +41,14 @@ def gather_data_local():
         header = list(final_data_dictionary.keys())
         writer = csv.DictWriter(file, fieldnames=header)
         writer.writeheader()
-        albums_obtained = []
+        # albums_obtained = []
 
         artists = get_artists_from_playlist(PLAYLIST)
 
         artists = list(artists.keys())
         for artist in artists:
             artist_name = spotify.artist(artist)['name']
+            print(f'Artist: {artist_name}')
 
             # get each album for the artist
             artists_albums = spotify.artist_albums(artist,album_type='album',limit=5)
@@ -63,10 +64,18 @@ def gather_data_local():
                         album_length = album_length + song['duration_ms']
 
                 # write the output to a csv that matches our desired format
-                writer.writerow({'Year Released': album['release_date'],
+                writer.writerow({'Year Released': album['release_date'][:4],
                                 'Album Length': album_length,
                                 'Album Name': album['name'],
                                 'Artist': artist_name})
+
+                # also write the output to our final data dictionary
+                final_data_dictionary['Year Released'].append(album['release_date'][:4])
+                final_data_dictionary['Album Length'].append(album_length)
+                final_data_dictionary['Album Name'].append(album['name'])
+                final_data_dictionary['Artist'].append(artist_name)
+                
+    return final_data_dictionary
 
 if __name__ == "__main__":
     data = gather_data_local()
