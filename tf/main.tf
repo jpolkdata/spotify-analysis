885eff7852a220bb1d18b489d868678c4c899bb0
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2022-12-30 16:13:04 -0600

    split s3 out to separate tf file and generate deployment zip

--- tf/main.tf
+++ tf/main.tf
@@ -1,33 +1,6 @@
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
-
-###############################
-# Bucket creation
-###############################
-resource "aws_s3_bucket" "spotify_bucket" {
-    bucket = "spotify-analysis-jpolkdata"
-}
-
-###############################
-# Bucket private access
-###############################
-resource "aws_s3_bucket_acl" "example" {
-    bucket = aws_s3_bucket.spotify_bucket.id
-    acl    = "private"
-}
-
-###############################
-# Disable bucket public access
-###############################
-resource "aws_s3_bucket_public_access_block" "protected_bucket_access" {
-    bucket = aws_s3_bucket.spotify_bucket.id
-
-    # Block public access
-    block_public_acls   = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+# create the lambda deployment package
+data "archive_file" "deployment" {
+  type        = "zip"
+  source_file = "../scripts/get_album_lengths_from_playlist.py"
+  output_path = "../deployment/get_album_lengths_from_playlist.zip"
 }
---
+++ tf/s3.tf
@@ -0,0 +1,23 @@
+#####################################################
+# S3 Bucket
+#####################################################
+resource "aws_s3_bucket" "spotify_bucket" {
+    bucket = "spotify-analysis-jpolkdata"
+}
+
+# Bucket private access
+resource "aws_s3_bucket_acl" "example" {
+    bucket = aws_s3_bucket.spotify_bucket.id
+    acl    = "private"
+}
+
+# Disable bucket public access
+resource "aws_s3_bucket_public_access_block" "protected_bucket_access" {
+    bucket = aws_s3_bucket.spotify_bucket.id
+
+    # Block public access
+    block_public_acls   = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
