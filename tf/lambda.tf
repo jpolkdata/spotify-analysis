4316adc208c088bb708a0204931adf1e8cf9bbf3
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2022-12-30 19:31:22 -0600

    lambda layers deployed from s3 via terraform

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -3,10 +3,10 @@
 #####################################################
 resource "aws_lambda_function" "spotify_analysis" {
   function_name = "spotify-analysis"
-  # layers = [
-  #   data.aws_lambda_layer_version.aws_sdk.arn,
-  #   data.aws_lambda_layer_version.requests_library.arn
-  # ]
+  layers = [
+    aws_lambda_layer_version.lambda_layer_requests.arn,
+    aws_lambda_layer_version.lambda_layer_spotipy.arn
+  ]
   filename = data.archive_file.lambda_deployment.output_path
   handler = "get_album_lengths_from_playlist.lambda_handler"
   role = "${aws_iam_role.lambda_execution_role.arn}"
@@ -24,19 +24,21 @@ resource "aws_lambda_function" "spotify_analysis" {
 #####################################################
 # Lambda layers
 #####################################################
-# data "aws_lambda_layer_version" "lambda_layer_requests" {
-#   filename            = "../deployment/layer_requests.zip"
-#   layer_name          = "python3-8-requests"
-#   # source_code_hash    = data.archive_file.lambda_layer_requests.output_base64sha256 # TODO: save the zip to s3
-#   compatible_runtimes = ["python3.8"]
-# }
+resource "aws_lambda_layer_version" "lambda_layer_requests" {
+  layer_name          = "python3-8-requests"
+  s3_bucket           = "${aws_s3_object.s3_layer_requests.bucket}"
+  s3_key              = "${aws_s3_object.s3_layer_requests.key}"
+  s3_object_version   = "${aws_s3_object.s3_layer_requests.version_id}"
+  compatible_runtimes = ["python3.8"]
+}
 
-# data "aws_lambda_layer_version" "lambda_layer_spotipy" {
-#   filename            = "../deployment/layer_spotipy.zip"
-#   layer_name          = "python3-8-spotipy"
-#   # source_code_hash    = data.archive_file.lambda_layer_spotipy.output_base64sha256 # TODO: save the zip to s3
-#   compatible_runtimes = ["python3.8"]
-# }
+resource "aws_lambda_layer_version" "lambda_layer_spotipy" {
+  layer_name          = "python3-8-spotipy"
+  s3_bucket           = "${aws_s3_object.s3_layer_spotipy.bucket}"
+  s3_key              = "${aws_s3_object.s3_layer_spotipy.key}"
+  s3_object_version   = "${aws_s3_object.s3_layer_spotipy.version_id}"
+  compatible_runtimes = ["python3.8"]
+}
 
 #####################################################
 # IAM role and policies
