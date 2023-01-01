d142a6e0573d0dbd1b32553ca4c1904947f13647
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2023-01-01 13:55:27 -0600

    move lambda layer bash script and rename it for clarity

--- deployment/create_lambda_layer_zips.sh
+++ deployment/create_lambda_layer_zips.sh
@@ -3,23 +3,20 @@
 ##################################################
 # Generate the lambda layer zip files
 ##################################################
+
 # remove the current layer zips and create a working folder
-rm -r deployment/layer*.zip
+rm -r layer*.zip
 mkdir -p python/
 
 # create the requests zip
 pip install requests -t python/
-zip -r deployment/layer_requests.zip python/
+zip -r layer_requests.zip python/
 rm -r python/*
 
 # create the spotipy zip
 pip install spotipy -t python/
-zip -r deployment/layer_spotipy.zip python/
+zip -r layer_spotipy.zip python/
 rm -r python/
 
-##################################################
-# Terraform deployment
-##################################################
-# cd tf
-# terraform init
-# terraform plan
+
+# NOTE: apply the terraform plan to deploy the files out to AWS
