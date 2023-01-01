75f22d72f9c248f62ba5ae3bbd5e2e659c10c622
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2023-01-01 10:14:25 -0600

    adding cloudwatch weekly trigger rule for lambda

---
+++ tf/cloudwatch.tf
@@ -0,0 +1,24 @@
+#####################################################
+# Cloudwatch rule
+#####################################################
+resource "aws_cloudwatch_event_rule" "weekly" {
+  name = "weekly"
+  description = "Trigger weekly"
+  schedule_expression = "rate(7 days)"
+  is_enabled = false
+}
+
+resource "aws_cloudwatch_event_target" "lambda" {
+  target_id = "spotify_analysis"
+  rule = "${aws_cloudwatch_event_rule.weekly.name}"
+  arn = "${aws_lambda_function.spotify_analysis.arn}"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_trigger_lambda" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.spotify_analysis.function_name
+  principal = "events.amazonaws.com"
+  source_arn = aws_cloudwatch_event_rule.every_week.arn
+}
+
