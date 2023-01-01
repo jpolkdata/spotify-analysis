79a6458ab9f3771208573ebcdfdaa746624187d3
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2023-01-01 11:31:47 -0600

    rename 'scripts' folder to 'source'

--- tf/main.tf
+++ tf/main.tf
@@ -1,6 +1,6 @@
 # create the lambda deployment package
 data "archive_file" "lambda_deployment" {
   type        = "zip"
-  source_file = "../scripts/get_album_lengths_from_playlist.py"
+  source_file = "../source/get_album_lengths_from_playlist.py"
   output_path = "../deployment/lambda_get_album_lengths_from_playlist.zip"
 }
