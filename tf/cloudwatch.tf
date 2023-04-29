0cd3e4517c01708e72164aea1e80b4d8ed5d0ad6
jpolkdata <40530465+jpolkdata@users.noreply.github.com>
2023-04-29 08:16:38 -0500

    Disabling the weekly cloudwatch trigger

--- tf/cloudwatch.tf
+++ tf/cloudwatch.tf
@@ -5,7 +5,7 @@ resource "aws_cloudwatch_event_rule" "weekly" {
   name = "weekly_spotify_analysis"
   description = "Trigger weekly"
   schedule_expression = "rate(7 days)"
-  is_enabled = true
+  is_enabled = false
   tags = {
     Project = local.project
   }
